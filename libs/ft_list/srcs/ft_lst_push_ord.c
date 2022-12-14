77d88024b3096706a776fc3c378d5ae5c457d65f
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2022-12-14 16:07:52 -0300

    Started implementation of push ordered function

---
+++ libs/ft_list/srcs/ft_lst_push_ord.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lst_push_ord.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 15:46:35 by lsilva-q          #+#    #+#             */
+/*   Updated: 2022/12/14 15:46:35 by lsilva-q         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+/* lesser_func must return if val1 < val2*/
+
+t_ftnode	*ft_lst_push_ord(t_ftlist *lst, void *content,
+	int (*lesser_func)(void *, void *))
+{
+	t_ftnode	*node;
+	t_ftnode	*iter;
+
+	if (lst == NULL || lesser_func == NULL)
+		return (NULL);
+	node = ft_node_new(content);
+	iter = lst->front;
+	while (iter != NULL)
+	{
+		if (!lesser_func(iter->content, content))
+		iter = iter->next;
+	}
+	return (NULL);
+}
