32dc874f38d580c08b565724adc7991fb4f36c16
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2022-12-28 12:40:02 -0300

    Added print_node function to debug ev_list

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -13,9 +13,37 @@
 #include "libft_x.h"
 #include "minishell.h"
 
+// void func(void *content, int is_first, int is_last);
+static void	print_node(void *content, int is_first, int is_last)
+{
+	t_ev_node	*node;
+
+	node = content;
+	if (is_first)
+		printf("[");
+	printf("(%s=%s)", node->name, node->value);
+	if (is_last)
+		printf("]");
+	else
+		printf(", ");
+}
+
 int	main(/*int argc, char *argv[]*/)
 {
+	core_init();
 	clear_screen();
+
+	printf("[ENVIRONMENT VARIABLES]\n\t");
+	ft_lst_func_apply(&(g_core.ev_list), print_node);
+	printf("\n");
+
+	set_ev("Test42", "424242", 0);
+	set_ev("TEST3", "333", 0);
+
+	printf("[ENVIRONMENT VARIABLES]\n\t");
+	ft_lst_func_apply(&(g_core.ev_list), print_node);
+	printf("\n");
+
 	handle_signal();
 	core_loop();
 	rl_clear_history();
