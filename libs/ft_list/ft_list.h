31bf9c52c21afb6e1afebcf721fbae5e57889076
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2022-12-29 08:48:15 -0300

    Include separation through comment categories

--- libs/ft_list/ft_list.h
+++ libs/ft_list/ft_list.h
@@ -33,27 +33,40 @@ typedef struct s_ftlist
 }	t_ftlist;
 
 // Public functions
+
 	// List initialization
+
 int			ft_lst_init(t_ftlist *lst);
+
 	// List cleaning
+
 void		ft_lst_clear(t_ftlist *lst, void (*f)(void *));
+
 	// Setget
+
 int			ft_lst_set(t_ftlist *lst, int index, void *content);
 void		*ft_lst_get(t_ftlist *lst, int index);
+
 	// Push
+
 t_ftnode	*ft_lst_push(t_ftlist *lst, int index, void *content);
 t_ftnode	*ft_lst_push_back(t_ftlist *lst, void *content);
 t_ftnode	*ft_lst_push_front(t_ftlist *lst, void *content);
+
 	// Pop
+
 void		*ft_lst_pop(t_ftlist *lst, int index);
 void		*ft_lst_pop_back(t_ftlist *lst);
 void		*ft_lst_pop_front(t_ftlist *lst);
+
 	// Utils
+
 t_ftlist	*ft_lst_dup(t_ftlist *lst, void *(*copy_f)(void *));
 int			ft_lst_swap_node(t_ftlist *lst, int index1, int index2);
 void		**ft_lst_toarray(t_ftlist *lst);
 
 // Private functions
+
 t_ftnode	*ft_lst_push_front_node(t_ftlist *lst, t_ftnode *node);
 t_ftnode	*ft_lst_push_back_node(t_ftlist *lst, t_ftnode *node);
 t_ftnode	*ft_lst_push_node(t_ftlist *lst, int index, t_ftnode *node);
@@ -64,10 +77,14 @@ void		ft_node_free(t_ftnode *node);
 t_ftnode	*ft_node_new(void *content);
 
 // Testing
+
 	// Push
+
 t_ftnode	*ft_lst_push_ord(t_ftlist *lst, void *content,
 	int (*lesser_func)(void *, void *));
+
 	// Utils
+
 t_ftnode	*ft_lst_find(t_ftlist *lst, void *value,
 	int (*cmp_func)(void *, void *));
 
