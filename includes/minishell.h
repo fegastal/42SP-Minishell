25d7e98bd4595a9920515386abf260bc876e2207
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2022-12-29 09:16:15 -0300

    Add tokenize enums and struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -23,12 +23,62 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <stdlib.h>
+# include "ft_list.h"
 
-void	core_loop(void);
-void	clear_screen(void);
-char	*get_prefix(void);
-void	handle_signal_fork(void);
-void	handle_signal(void);
-char	*create_prompt(void);
+# define ALL_TOKEN_CHARS " |$?'\"<>-"
+
+typedef enum e_token_modes
+{
+	TKMODE_DEFAULT,
+	TKMODE_STR_SINGLE,
+	TKMODE_STR_DOUBLE,
+}	t_token_modes;
+
+// 00) TKTYPE_WHITESPACE,
+// 01) TKTYPE_PIPE,
+// 02) TKTYPE_DOLLAR,
+// 03) TKTYPE_QUESTION_MARK,
+// 04) TKTYPE_STR_SINGLE,
+// 05) TKTYPE_STR_DOUBLE,
+// 06) TKTYPE_ABRACK_LEFT,
+// 07) TKTYPE_ABRACK_RIGHT,
+// 08) TKTYPE_MINUS,
+// 09) TKTYPE_END,
+// 10) TKTYPE_INVALID
+typedef enum e_token_types
+{
+	TKTYPE_WHITESPACE,
+	TKTYPE_PIPE,
+	TKTYPE_DOLLAR,
+	TKTYPE_QUESTION_MARK,
+	TKTYPE_STR_SINGLE,
+	TKTYPE_STR_DOUBLE,
+	TKTYPE_ABRACK_LEFT,
+	TKTYPE_ABRACK_RIGHT,
+	TKTYPE_MINUS,
+	TKTYPE_END,
+	TKTYPE_INVALID
+}	t_token_types;
+
+typedef struct	s_msh_token
+{
+	char	*ptr;
+	int		type;
+}	t_msh_token;
+
+void		core_loop(void);
+void		clear_screen(void);
+char		*get_prefix(void);
+void		handle_signal_fork(void);
+void		handle_signal(void);
+char		*create_prompt(void);
+
+// Parser
+
+int			get_token_type(char chr);
+t_ftlist	tokenize(char *str);
+
+	// Debug
+char		*get_token_name(int token_type);
 
 #endif
