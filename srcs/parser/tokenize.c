c01d83789edf2770f49f8be4598b1d9ceda89689
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2022-12-29 12:58:41 -0300

    Change return type from list to array of lists
    Each item from array is a list containing all tokens from same type

--- srcs/parser/tokenize.c
+++ srcs/parser/tokenize.c
@@ -12,34 +12,55 @@
 
 #include "minishell.h"
 
-void	place_token(t_ftlist *list, char *ptr, int type);
+static void	init_array(t_ftlist **arr);
+static void	place_token(t_ftlist *list, char *ptr, int type);
 
-t_ftlist	tokenize(char *str)
+/*
+	Return a token array, containing all tokens from a string. Each index from
+	the array represents a token from t_token_types (except TKTYPE_INVALID).
+*/
+t_ftlist	*tokenize(char *str)
 {
 	char		*iter;
 	int			type;
-	t_ftlist	list;
+	t_ftlist	*arr;
 
-	ft_lst_init(&list);
+	if (str == NULL)
+		return (NULL);
+	init_array(&arr);
+	place_token(arr, str, TKTYPE_START);
 	iter = str;
 	while (*iter != '\0')
 	{
 		type = get_token_type(*iter);
 		if (type != TKTYPE_INVALID)
-			place_token(&list, iter, type);
+			place_token(arr, iter, type);
 		iter++;
 	}
-	return (list);
+	place_token(arr, iter, TKTYPE_END);
+	return (arr);
 }
 
-void	place_token(t_ftlist *list, char *ptr, int type)
+static void	init_array(t_ftlist **arr)
+{
+	int	i;
+
+	*arr = malloc(sizeof(t_ftlist) * TOKENS_COUNT);
+	i = 0;
+	while (i < TOKENS_COUNT)
+	{
+		// printf("%i -> %i\n", i, ft_lst_init(*arr + i));
+		ft_lst_init(*arr + i);
+		i += 1;
+	}
+}
+
+static void	place_token(t_ftlist *arr, char *ptr, int type)
 {
 	t_msh_token	*token;
 
-	if (list == NULL)
-		return ;
 	token = malloc(sizeof(t_msh_token));
 	token->ptr = ptr;
 	token->type = type;
-	ft_lst_push_back(list, token);
+	ft_lst_push_back(arr + type, token);
 }
