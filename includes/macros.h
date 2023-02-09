aaa6801b63a6956ca445bb0e78c5335ac16a8c32
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-02-08 21:15:58 -0300

    Create macros.h

---
+++ includes/macros.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   macros.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 11:33:18 by lsilva-q          #+#    #+#             */
+/*   Updated: 2023/02/08 11:33:18 by lsilva-q         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACROS_H
+# define MACROS_H
+
+// [PENDENTE]: Unificar todas as macros neste arquivo
+
+typedef enum	e_mode
+{
+	DEFAULT,
+	DOUBLE_QUOTES,
+	SINGLE_QUOTES
+}	t_mode;
+
+# endif
