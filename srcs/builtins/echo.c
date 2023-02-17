c39a36096a00027c8e28ce444d3959ca31c74c49
Fernanda Gastal <fernanda.gastal.figueiredo@usp.br>
2023-02-17 20:22:57 -0300

    Update echo.c

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -17,5 +17,34 @@
 	-n	do not append a newline
 */
 
-#include "minishell.h"
+#include "builtins.h"
 
+void echo(t_cmd *cmd)
+{
+	char	**iter;
+	t_echo	e;
+
+	e.find_flag = 1;
+	e.ignore_newline = 0;
+	e.final_str = ft_strdup("");
+	iter = cmd->args + 1;
+	if ((*iter)[0] == '-')
+	{
+		if
+	}
+
+// echo -E "dhuda" -n "ddjid"
+
+// set_flags -> percorre todas as strings do cmd,
+// e procura por flags, setando na struct t_echo
+// passada por parâmetro
+
+// ft_strchr -> utilizar para validar se todos os
+// caracteres da flag fazem parte do conjunto de
+// caracteres válidos
+
+// Criar uma lista para inserir as strings que serão
+// impressas (linhas de texto e linhas de flags inválidadas)
+// Ex: echo -nex "test" -> "-nex test"
+
+}
