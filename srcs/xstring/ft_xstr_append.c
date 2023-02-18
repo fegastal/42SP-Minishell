2e2557a62ef783d39eb048add001c4a91f33e7cf
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-02-18 11:35:55 -0300

    Implement append strings function

---
+++ srcs/xstring/ft_xstr_append.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_xstr_append.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 10:09:51 by lsilva-q          #+#    #+#             */
+/*   Updated: 2023/02/18 10:09:51 by lsilva-q         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "xstring.h"
+
+char	*ft_xstr_append(char const *str1, char const *str2)
+{
+	char	*new_str;
+	size_t	len;
+
+	if (str1 == NULL && str2 == NULL)
+		return (NULL);
+	else if (str1 == NULL)
+		return (ft_strdup(str2));
+	else if (str2 == NULL)
+		return (ft_strdup(str1));
+	len = ft_strlen(str1) + ft_strlen(str2) + 1;
+	new_str = malloc(sizeof(char) * len);
+	ft_strlcpy(new_str, str1, len);
+	ft_strlcat(new_str, str2, len);
+	return (new_str);
+}
