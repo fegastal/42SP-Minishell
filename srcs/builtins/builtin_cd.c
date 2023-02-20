fc21f76f2f01dfc1608b0e283448a285f0a9ccf7
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-02-20 08:43:44 -0300

    Remove old commented code

--- srcs/builtins/builtin_cd.c
+++ srcs/builtins/builtin_cd.c
@@ -28,15 +28,11 @@ void	builtin_cd(t_cmd *cmd)
 		|| !ft_strcmp(home_ev->value, "~/"))
 	{
 		tmp = ft_strdup(home_ev->value);
-		// if (chdir(home_ev->value) == -1)
-		// 	return ;	// Mudar por erro (no such file or directory)
 	}
 	else
 	{
 		cwd = getcwd(NULL, 0);
 		tmp = ft_xstr_join("/", cwd, cmd->args[1]);
-		// if (chdir(ft_xstr_join("/", cwd, cmd->args[1])) == -1)
-		// 	return ;	// Mudar por erro (no such file or directory)
 		free(cwd);
 	}
 	if (chdir(tmp) == -1)
@@ -46,32 +42,3 @@ void	builtin_cd(t_cmd *cmd)
 	}
 	set_ev("PWD", tmp);
 }
-
-// void	builtin_cd(t_cmd *cmd)
-// {
-// 	char	*target_path;
-// 	t_ev	*home_ev;
-// 	t_ev	*pwd_ev;
-
-// 	if (cmd->args_count > 2)
-// 		return ;	// Mudar por erro (too many arguments)
-// 	home_ev = get_ev("HOME");
-// 	if (home_ev == NULL)
-// 		return ;	// Mudar por erro (HOME not set)
-// 	else if (cmd->args_count == 1)
-// 		target_path = ft_strdup(home_ev->value);
-// 	else if (cmd->args[1][0] == '~')
-// 		target_path = ft_strjoin(home_ev->value, cmd->args[1] + 1);
-// 	else
-// 	{
-// 		pwd_ev = get_ev("PWD");
-// 		if (pwd_ev == NULL)
-// 			return ;	// Mudar por erro (PWD not set)
-// 		target_path = ft_xstr_join("/", pwd_ev->value, cmd->args[1]);
-// 	}
-// 	if (chdir(target_path) == -1)
-// 		return ;	// Mudar por erro (no such file or directory)
-// 	printf("target_path: %s\n", target_path);
-// 	chdir(target_path);
-// 	set_ev("PWD", target_path);
-// }
