96df9c81b97f6f1c199b3dd064983adffe6d76f3
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-03-05 17:37:37 -0300

    Fix ft_strtrim return when string has only chars from set

--- libs/libft_x/libft_gnl/ft_strtrim.c
+++ libs/libft_x/libft_gnl/ft_strtrim.c
@@ -12,50 +12,52 @@
 
 #include "libft_x.h"
 
-static size_t	ft_find_start(char const *s1, char const *set)
-{
-	size_t	i;
-	size_t	s1_len;
-
-	i = 0;
-	s1_len = ft_strlen(s1);
-	while (i < s1_len)
-	{
-		if (!ft_strchr(set, s1[i]))
-			return (i);
-		i += 1;
-	}
-	return (0);
-}
-
-static size_t	ft_find_end(char const *s1, char const *set)
-{
-	size_t	i;
-
-	i = ft_strlen(s1) - 1;
-	while (i > 0)
-	{
-		if (!ft_strchr(set, s1[i]))
-			return (i);
-		i -= 1;
-	}
-	return (0);
-}
+static char const	*ft_find_start(char const *s1, char const *set);
+static char const	*ft_find_end(char const *s1, char const *set);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	start;
-	size_t	end;
-	char	*newstr;
+	char const	*start;
+	char const	*end;
+	char		*newstr;
 
-	if (s1)
+	if (s1 != NULL)
 	{
 		start = ft_find_start(s1, set);
 		end = ft_find_end(s1, set);
-		newstr = ft_substr(s1, start, (end - start) + 1);
+		newstr = ft_strndup(start, (end - start) + 1);
 		if (!newstr)
 			return (NULL);
 		return (newstr);
 	}
 	return (NULL);
 }
+
+static char const	*ft_find_start(char const *s1, char const *set)
+{
+	char const	*iter;
+
+	iter = s1;
+	while (*iter != '\0')
+	{
+		if (!ft_strchr(set, *iter))
+			return (iter);
+		iter++;
+	}
+	return (iter);
+}
+
+static char const	*ft_find_end(char const *s1, char const *set)
+{
+	char const	*iter;
+
+	iter = s1 + ft_strlen(s1) - 1;
+	while (iter > s1)
+	{
+		if (!ft_strchr(set, *iter))
+			return (iter);
+		iter--;
+	}
+	return (iter);
+}
+
