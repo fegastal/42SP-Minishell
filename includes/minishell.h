db768cecfb120572549285d3c068ca61d87d005a
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-03-04 22:48:40 -0300

    Reimplement redir context parser

--- includes/minishell.h
+++ includes/minishell.h
@@ -43,6 +43,13 @@ typedef struct	s_cmd_executor
 	int		wstatus;
 }	t_cmd_executor;
 
+typedef struct	s_redir_context
+{
+	t_redir_slice	*first_cmd;
+	t_redir_slice	*first_infile;
+	t_redir_slice	*last_outfile;
+}	t_redir_context;
+
 void	main_loop(void);
 void	clear_screen(void);
 char	*get_prefix(void);
--- srcs/parser/redir_split_line.c
+++ srcs/parser/redir_split_line.c
@@ -17,16 +17,16 @@ static void	end_func(t_splitter *sp);
 
 t_ftlist	redir_split_line(const char *line)
 {
-	// t_ftlist	tokens;
-	// t_ftlist	slices;
 	t_ftlist	redirs;
+	t_redirs	last_type;
 
-	redirs = parse_context(line, (t_context){
+	last_type = REDIR_NONE;
+	redirs = parse_context(line, (t_context){	// Não esquecer de tratar as aspas
 			.def_func = default_mode,
 			.dquotes_func = NULL,
 			.squotes_func = NULL,
 			.end_func = end_func
-		});
+		}, &last_type);
 
 	return (redirs);
 }
@@ -36,47 +36,74 @@ t_ftlist	redir_split_line(const char *line)
 // Iter        // #
 // mode = DEFAULT
 
+//              ls -l > outfile.txt ls -l | pwd > path.txt
+//
+// PIPE[0] -> ls -l > outfile.txt ls -l
+//		REDIR[0] (CMD) = "ls -l"
+//		REDIR[1] (OUT) = "outfile.txt"
+//		REDIR[2] (CMD) = "ls -l"
+// PIPE[1] -> pwd > path.txt
+//		REDIR[0] (CMD) = "pwd"
+//		REDIR[1] (OUT) = "path.txt"
+
+
+
 // [PENDENTE]: Implementar separação de pedaços da string da linha, em pequenos pedaços
+
+// mode = DEFAULT
+// last_type = REDIR_NONE
+// last_found:               *
+//             ls -l > file1 > file2>file3>file4 cmd1 args args
+// iter:                     ^
+/* list = {
+	(REDIR_CMD, "ls -l "),
+	(REDIR_OUT, "file1 "),
+}*/
 
 static void	default_mode(t_splitter *sp)
 {
-	t_slice	*slice;
+	t_redir_slice	*slice;
+	t_redirs		*last_type;
 
-	slice = malloc(sizeof(t_slice));
-	slice->type = REDIR_NONE;
-	if (*sp->iter == '"')
-		sp->mode = DOUBLE_QUOTES;
-	else if (*sp->iter == '\'')
-		sp->mode = SINGLE_QUOTES;
-	else if (*sp->iter == '<')
+	last_type = (t_redirs *) sp->aux;
+	if (sp->last_found == NULL && *sp->iter != ' ' && *sp->iter != '>')
+		sp->last_found = sp->iter;
+	if (*sp->iter == ' ' && sp->last_found != NULL && *last_type == REDIR_OUT)
 	{
-		if (*(sp->iter + 1) == '<')
-			slice->type = REDIR_HEREDOC;
-		else
-			slice->type = REDIR_IN;
-	}
-	else if (*sp->iter == '>')
-	{
-		if (*(sp->iter + 1) == '>')
-			slice->type = REDIR_APPEND;
-		else
-			slice->type = REDIR_OUT;
-	}
-	if (slice->type == REDIR_NONE)
-		free(slice);
-	else
-	{
-		slice->start = (char *) sp->iter;	// Temporário (alocar antes de atribuir) Obs: Não sei se é pra alocar aqui mesmo, mas precisa alocar
-		if (slice->type == REDIR_APPEND || slice->type == REDIR_HEREDOC)
-			sp->iter++;
+		slice = malloc(sizeof(t_redir_slice));
+		slice->fd = -1;
+		slice->type = *last_type;
+		slice->str = ft_strndup(sp->last_found, sp->iter - sp->last_found);
 		ft_lst_push_back(&(sp->list), slice);
+		sp->last_found = NULL;
+		*last_type = REDIR_NONE;
 	}
+	else if (*sp->iter == '>' && *last_type != REDIR_NONE)
+	{
+		slice = malloc(sizeof(t_redir_slice));
+		slice->fd = -1;
+		slice->type = *last_type;
+		slice->str = ft_strndup(sp->last_found, sp->iter - sp->last_found);
+		ft_lst_push_back(&(sp->list), slice);
+		sp->last_found = NULL;
+		*last_type = REDIR_OUT;
+	}
+	else if (*last_type == REDIR_NONE)
+		*last_type = REDIR_CMD;
 }
 
-/* This function does nothing (but it's not useless) :) */
 static void	end_func(t_splitter *sp)
 {
-	if (sp)
-		return ;
-	return ;
+	t_redir_slice		*slice;
+	t_redirs	*last_type;
+
+	last_type = (t_redirs *) sp->aux;
+	if (sp->last_found != NULL)
+	{
+		slice = malloc(sizeof(t_redir_slice));
+		slice->fd = -1;
+		slice->type = *last_type;
+		slice->str = ft_strndup(sp->last_found, sp->iter - sp->last_found);
+		ft_lst_push_back(&(sp->list), slice);
+	}
 }
