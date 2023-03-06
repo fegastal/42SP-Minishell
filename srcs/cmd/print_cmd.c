bc16d9a44cbd520fe9ed9cdaeaa06d21973174a0
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-03-05 21:21:25 -0300

    Reimplement print_cmd to debug passing fd

--- srcs/cmd/print_cmd.c
+++ srcs/cmd/print_cmd.c
@@ -12,20 +12,40 @@
 
 #include "cmd.h"
 
-void	print_cmd(t_cmd *cmd)
+void	print_cmd(t_cmd *cmd, int fd)
 {
 	char	**tmp;
 
-	printf("{\n  path: %s,\n  args: [", cmd->path);
+	ft_putstr_fd("{\n  path: ", fd);
+	if (cmd->path != NULL)
+	{
+		ft_putchar_fd('"', fd);
+		ft_putstr_fd(cmd->path, fd);
+		ft_putchar_fd('"', fd);
+	}
+	else
+		ft_putstr_fd("NULL", fd);
+	ft_putstr_fd(",\n  args: [", fd);
+	// printf("{\n  path: %s,\n  args: [", cmd->path);
 	tmp = cmd->args;
 	while (*tmp != NULL)
 	{
-		printf("\n    %s", *tmp);
+		ft_putstr_fd("\n    \"", fd);
+		ft_putstr_fd(*tmp, fd);
+		ft_putstr_fd("\"", fd);
+		// printf("\n    %s", *tmp);
 		if (*(tmp + 1) != NULL)
-			printf(",");
+			ft_putstr_fd(",", fd);
+			// printf(",");
 		else
-			printf("\n  ");
+			ft_putstr_fd("\n  ", fd);
+			// printf("\n  ");
 		tmp++;
 	}
-	printf("]\nargs_count: %d\n}\n", cmd->args_count);
+	ft_putstr_fd("],\n  args_count: ", fd);
+	ft_putnbr_fd(cmd->args_count, fd);
+	ft_putstr_fd("\n  is_builtin: ", fd);
+	ft_putnbr_fd(cmd->is_builtin, fd);
+	ft_putstr_fd("\n}\n", fd);
+	// printf("]\nargs_count: %d\n}\n", cmd->args_count);
 }
