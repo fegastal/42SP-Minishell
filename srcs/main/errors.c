8df2b08ab4ea1b88d3888d58e6dd158ad5c697c6
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-03-08 21:34:44 -0300

    Implement specific error functions

---
+++ srcs/main/errors.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_syntax.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 19:51:17 by lsilva-q          #+#    #+#             */
+/*   Updated: 2023/03/08 19:51:17 by lsilva-q         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	syntax_error(void)
+{
+	error(ERR_CUSTOM_ERROR, "Error: Syntax error\n");
+	g_core.last_status = ERR_SYNTAX_ERROR;
+	g_core.can_proceed = 0;
+}
+
+void	file_permission_error(void)
+{
+	error(ERR_FILE_NO_PERMISSION, NULL);
+	g_core.last_status = ERR_SYNTAX_ERROR;
+	g_core.can_proceed = 0;
+}
