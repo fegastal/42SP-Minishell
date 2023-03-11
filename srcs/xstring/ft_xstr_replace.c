f805c1ece3f7a4022f1c6132c35f2d9004dc5f35
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-03-11 19:27:08 -0300

    Refactor according to Norme

--- srcs/xstring/ft_xstr_replace.c
+++ srcs/xstring/ft_xstr_replace.c
@@ -12,13 +12,13 @@
 
 #include "xstring.h"
 
+static void	loop(const char **str_ptr, const char *old_ptr,
+				const char *new, char **new_str);
+
 char	*ft_xstr_replace(const char *str, const char *old, const char *new)
 {
-	const char	*old_ptr;
 	const char	*str_ptr;
-	const char	*aux;
 	char		*new_str;
-	char		*temp;
 
 	if (str == NULL)
 		return (NULL);
@@ -26,27 +26,33 @@ char	*ft_xstr_replace(const char *str, const char *old, const char *new)
 		return (ft_strdup(str));
 	new_str = ft_strdup("");
 	str_ptr = str;
-	while (*str_ptr)
-	{
-		old_ptr = old;
-		aux = str_ptr;
-		while (*old_ptr != '\0' && *aux == *old_ptr)
-		{
-			aux++;
-			old_ptr++;
-		}
-		if (*old_ptr == '\0')
-		{
-			ft_xstr_supplant(&new_str, ft_xstr_append(new_str, new));
-			str_ptr = aux;
-		}
-		else
-		{
-			temp = ft_strndup(str_ptr, 1);
-			ft_xstr_supplant(&new_str, ft_xstr_append(new_str, temp));
-			free(temp);
-			str_ptr++;
-		}
-	}
+	while (*str_ptr != '\0')
+		loop(&str_ptr, old, new, &new_str);
 	return (new_str);
+}
+
+static void	loop(const char **str_ptr, const char *old_ptr,
+				const char *new, char **new_str)
+{
+	const char	*aux;
+	char		*temp;
+
+	aux = *str_ptr;
+	while (*old_ptr != '\0' && *aux == *old_ptr)
+	{
+		aux++;
+		old_ptr++;
+	}
+	if (*old_ptr == '\0')
+	{
+		ft_xstr_supplant(new_str, ft_xstr_append(*new_str, new));
+		*str_ptr = aux;
+	}
+	else
+	{
+		temp = ft_strndup(*str_ptr, 1);
+		ft_xstr_supplant(new_str, ft_xstr_append(*new_str, temp));
+		free(temp);
+		(*str_ptr)++;
+	}
 }
