d49b049007fa0f443f7b45fb60d9ba893a51ee43
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-03-12 12:21:23 -0300

    Remove old comments

--- srcs/cmd/get_cmd_path.c
+++ srcs/cmd/get_cmd_path.c
@@ -39,22 +39,3 @@ char	*get_cmd_path(const char *cmd_name)
 	}
 	return (NULL);
 }
-
-// char	*get_cmd_path(const char *cmd_name, char **paths)
-// {
-// 	char	**iter;
-// 	char	*path;
-
-// 	if (!cmd_name || !paths)
-// 		return (NULL);
-// 	iter = paths;
-// 	while (*iter != NULL)
-// 	{
-// 		path = ft_xstr_join("/", 2, *iter, cmd_name);
-// 		if (!access(path, X_OK))
-// 			return (path);
-// 		free(path);
-// 		iter++;
-// 	}
-// 	return (NULL);
-// }
