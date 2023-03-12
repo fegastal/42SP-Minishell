b315368fd5bb3c66dcf2adc9b6d49fce5a4e1450
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-03-12 12:27:38 -0300

    Refactor code

--- srcs/main/cmd_stack/exec_cmd.c
+++ srcs/main/cmd_stack/exec_cmd.c
@@ -12,9 +12,8 @@
 
 #include "minishell.h"
 
-// Última tarefa em progresso: Execução de built-in com pipe
-
 static void	child_process(t_cmd_executor *executor);
+static void	handle_child_redirs(t_cmd_executor *executor);
 
 void	exec_cmd(t_cmd *cmd, int is_first, int is_last)
 {
@@ -39,6 +38,28 @@ void	exec_cmd(t_cmd *cmd, int is_first, int is_last)
 
 static void	child_process(t_cmd_executor *executor)
 {
+	handle_child_redirs(executor);
+	if (executor->cmd->is_builtin)
+		executor->wstatus = call_builtin(executor->cmd);
+	else
+	{
+		executor->wstatus = execve(
+				executor->cmd->path,
+				executor->cmd->args, g_core.envp);
+	}
+	if (executor->wstatus == -1)
+	{
+		if (!executor->cmd->is_builtin && executor->cmd->path == NULL)
+		{
+			error(ERR_CMD_NOT_FOUND, executor->cmd->args[0]);
+			exit(ERR_CMD_NOT_FOUND);
+		}
+		exit(ERR_FAILURE);
+	}
+}
+
+static void	handle_child_redirs(t_cmd_executor *executor)
+{
 	if (g_core.fd_in_type == FD_REDIR_STD)
 		dup2(g_core.std_in, STDIN_FILENO);
 	else if (g_core.fd_in_type == FD_REDIR_FILE)
@@ -55,135 +76,4 @@ static void	child_process(t_cmd_executor *executor)
 		exit(ERR_FAILURE);
 	else if (g_core.fd_in == -1 && g_core.fd_in_type == FD_REDIR_FILE)
 		exit(ERR_FAILURE);
-	if (executor->cmd->is_builtin)
-		executor->wstatus = call_builtin(executor->cmd);
-	else
-	{
-		executor->wstatus = execve(
-				executor->cmd->path,
-				executor->cmd->args, g_core.envp);
-	}
-	if (executor->wstatus == -1)
-	{
-		if (!executor->cmd->is_builtin && executor->cmd->path == NULL)
-		{
-			error(ERR_CMD_NOT_FOUND, executor->cmd->args[0]);
-			exit(ERR_CMD_NOT_FOUND);
-		}
-		exit(ERR_FAILURE); // Tratar erro de comando inválido
-	}
 }
-
-// [PENDENTE]: Paramos aqui! Revisar redirecionamentos com pipe (aparentemente o
-// descritor do pipe não está executando direito, pode ser que esteja sendo
-// fechado ou substituido por outro descritor antes de ser usado)
-
-// static void	child_process(t_cmd_executor *executor)
-// {
-// 	dup2(g_core.fd_in, STDIN_FILENO);
-// 	dup2(g_core.fd_out, STDOUT_FILENO);
-// 	if (g_core.fd_out == -1)
-// 		exit(EXIT_FAILURE);	// Erro de descritor inválido (provavelmente não aberto)
-// 	if (executor->cmd->is_builtin)
-// 		executor->wstatus = call_builtin(executor->cmd);
-// 	else
-// 		executor->wstatus = execve(
-// 				executor->cmd->path,
-// 				executor->cmd->args, g_core.envp);
-// 	if (executor->wstatus == -1)
-// 		exit(EXIT_FAILURE); // Tratar erro de comando inválido
-// }
-
-// static void	child_process(t_cmd_executor *executor)
-// {
-// 	if (!executor->is_first)
-// 	{
-// 		dup2(executor->last_pipe_in, STDIN_FILENO);
-// 		close(executor->last_pipe_in);
-// 	}
-// 	if (executor->is_first && executor->is_last)
-// 	{
-// 		dup2(g_core.fd_in, STDIN_FILENO);
-// 		dup2(g_core.fd_out, STDOUT_FILENO);
-// 		// dup2(g_core.std_in, STDIN_FILENO);
-// 		// dup2(g_core.std_out, STDOUT_FILENO);
-// 	}
-// 	else if (executor->is_last)
-// 		dup2(g_core.fd_out, STDOUT_FILENO);
-// 		// dup2(g_core.std_out, STDOUT_FILENO);
-// 	else if (!executor->is_last)
-// 	{
-// 		dup2(g_core.pipe[1], STDOUT_FILENO);	// Revisar linha (está redirecionando sempre para a saída do pipe)
-// 		close(g_core.pipe[1]);
-// 	}
-// 	if (executor->cmd->is_builtin)
-// 		executor->wstatus = call_builtin(executor->cmd);
-// 	else
-// 		executor->wstatus = execve(
-// 				executor->cmd->path,
-// 				executor->cmd->args, g_core.envp);
-// 	if (executor->wstatus == -1)
-// 		exit(EXIT_FAILURE); // Tratar erro de comando inválido
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// void exec_cmd(t_cmd *cmd, int is_first, int is_last)
-// {
-// 	int		wstatus;
-// 	int		tmp;
-
-// 	wstatus = -1;
-// 	tmp = g_core.pipe[0];
-// 	if (pipe(g_core.pipe) == -1)
-// 		exit(EXIT_FAILURE);
-// 	g_core.last_pid = fork();
-// 	if (g_core.last_pid == 0)
-// 	{
-// 		if (!is_first)
-// 		{
-// 			dup2(tmp, STDIN_FILENO);
-// 			close(tmp);
-// 		}
-// 		if (is_first && is_last)
-// 		{
-// 			dup2(g_core.std_in, STDIN_FILENO);
-// 			dup2(g_core.std_out, STDOUT_FILENO);
-// 		}
-// 		else if (is_last)
-// 			dup2(g_core.std_out, STDOUT_FILENO);
-// 		else if (!is_last)
-// 		{
-// 			dup2(g_core.pipe[1], STDOUT_FILENO);
-// 			close(g_core.pipe[1]);
-// 		}
-// 		if (is_builtin(cmd->args[0]))
-// 			wstatus = call_builtin(cmd);
-// 		else
-// 			wstatus = execve(cmd->path, cmd->args, g_core.envp);
-// 		if (wstatus == -1)
-// 			exit(EXIT_FAILURE);	// Tratar erro de comando inválido
-// 	}
-// 	close(g_core.pipe[1]);
-// 	waitpid(g_core.last_pid, &wstatus, 0);
-// 	g_core.last_status = WEXITSTATUS(wstatus);
-// }
