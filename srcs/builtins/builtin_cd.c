904f2bf771f5b54e4301367627f49538a4910004
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-03-12 12:10:05 -0300

    Refactor builtin_cmd code

--- srcs/builtins/builtin_cd.c
+++ srcs/builtins/builtin_cd.c
@@ -12,40 +12,81 @@
 
 #include "builtins.h"
 
+static char	*get_new_pwd(char *cwd, const char *home_path, t_cmd *cmd);
+
 int	builtin_cd(t_cmd *cmd)
 {
-	t_ev	*home_ev;
+	char	*home_path;
 	char	*cwd;
 	char	*tmp;
 
 	if (cmd->args_count > 2)
-	{
-		error(ERR_WRONG_BUILTIN_ARGS, NULL);
-		return (ERR_WRONG_BUILTIN_ARGS);
-	}
-	home_ev = get_ev("HOME");
-	if (home_ev == NULL)
-	{
-		error(ERR_HOME_NOT_SET, NULL);
-		return (ERR_HOME_NOT_SET);
-	}
+		return (wrong_builtin_args_error());
+	home_path = get_ev_value("HOME");
+	if (home_path == NULL)
+		return (home_not_set_error());
 	cwd = getcwd(NULL, 0);
-	if (cmd->args_count == 1)
-		tmp = ft_strdup(home_ev->value);
-	else
-	{
-		if (!ft_strcmp(cmd->args[1], "~") || !ft_strcmp(home_ev->value, "~/"))
-			tmp = ft_strdup(home_ev->value);
-		else
-			tmp = ft_xstr_join("/", cwd, cmd->args[1]);
-	}
+	tmp = get_new_pwd(cwd, home_path, cmd);
 	set_ev(ft_strdup("OLDPWD"), cwd);
 	if (chdir(tmp) == -1)
 	{
 		free(tmp);
-		error(ERR_NO_SUCH_FILE_OR_DIR, NULL);
-		return (ERR_NO_SUCH_FILE_OR_DIR);
+		return (no_such_file_or_dir_error());
 	}
 	set_ev("PWD", tmp);
 	return (ERR_NO_ERROR);
 }
+
+static char	*get_new_pwd(char *cwd, const char *home_path, t_cmd *cmd)
+{
+	char	*tmp;
+
+	if (cmd->args_count == 1)
+		tmp = ft_strdup(home_path);
+	else
+	{
+		if (!ft_strcmp(cmd->args[1], "~") || !ft_strcmp(home_path, "~/"))
+			tmp = ft_strdup(home_path);
+		else
+			tmp = ft_xstr_join("/", cwd, cmd->args[1]);
+	}
+	return (tmp);
+}
+
+// int	builtin_cd(t_cmd *cmd)
+// {
+// 	t_ev	*home_ev;
+// 	char	*cwd;
+// 	char	*tmp;
+
+// 	if (cmd->args_count > 2)
+// 	{
+// 		error(ERR_WRONG_BUILTIN_ARGS, NULL);
+// 		return (ERR_WRONG_BUILTIN_ARGS);
+// 	}
+// 	home_ev = get_ev("HOME");
+// 	if (home_ev == NULL)
+// 	{
+// 		error(ERR_HOME_NOT_SET, NULL);
+// 		return (ERR_HOME_NOT_SET);
+// 	}
+// 	cwd = getcwd(NULL, 0);
+// 	if (cmd->args_count == 1)
+// 		tmp = ft_strdup(home_ev->value);
+// 	else
+// 	{
+// 		if (!ft_strcmp(cmd->args[1], "~") || !ft_strcmp(home_ev->value, "~/"))
+// 			tmp = ft_strdup(home_ev->value);
+// 		else
+// 			tmp = ft_xstr_join("/", cwd, cmd->args[1]);
+// 	}
+// 	set_ev(ft_strdup("OLDPWD"), cwd);
+// 	if (chdir(tmp) == -1)
+// 	{
+// 		free(tmp);
+// 		error(ERR_NO_SUCH_FILE_OR_DIR, NULL);
+// 		return (ERR_NO_SUCH_FILE_OR_DIR);
+// 	}
+// 	set_ev("PWD", tmp);
+// 	return (ERR_NO_ERROR);
+// }
